1d9714597840656ca54ed08518a45ce4bf524a9c
flypirate <albertogmj@outlook.es>
2026-08-17 19:25:14 +0000

    lol

--- cub3D/src/exec/init_exec.c
+++ cub3D/src/exec/init_exec.c
@@ -1,22 +1,33 @@
 #include "cub3d.h"
 
 
-int init_exec(int argc, char **argv)
+int init_exec(int argc, char **argv, t_framework *fw)
 {
-
-
+	if (init_mlx(fw) == 1)
+		return (1);
+	if (run_mlx(fw) == 1)
+		return (1);
 
 
     return (0);
 }
 
 
-int	init_mlx(t_mlx *fw)
+int	init_mlx(t_framework *fw)
 {
 
-	fw->mlx.mlx =
-	fw->mlx.win = 
+	fw->mlx.mlx = mlx_init();
+	if (!fw->mlx.mlx)
+		return (1);
+	fw->mlx.win = mlx_new_window(fw->mlx.mlx, 1920, 1080, "cube3d");
+	fw->mlx.frame = mlx_new_image(fw->mlx.mlx, 1920, 1080);
+	if (!fw->mlx.frame)
+		return (1);
+	return (0);
+}
 
-
+int run_mlx(t_framework *fw)
+{
+	mlx_loop(fw->mlx.mlx);
 	return (0);
 }
--- cub3D/src/main.c
+++ cub3D/src/main.c
@@ -18,5 +18,7 @@ int main(int argc, char **argv)
 	fw = parser(argc, argv);
 	if (!fw)
 		return (0);
+	else
+		init_exec(argc, argv, fw);
 	return (0);
 }
